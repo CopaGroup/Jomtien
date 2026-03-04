b6d6362160921fc2d0123fe44e549bde4ae5731f
CopaGroup <copamkty@gmail.com>
2026-03-04 15:51:09 +0700

    Create translations.js file for multi-language support

---
+++ translations.js
@@ -0,0 +1,18 @@
+// translations.js
+
+const translations = {
+    en: {
+        greeting: "Hello",
+        farewell: "Goodbye"
+    },
+    es: {
+        greeting: "Hola",
+        farewell: "Adiós"
+    },
+    fr: {
+        greeting: "Bonjour",
+        farewell: "Au revoir"
+    }
+};
+
+export default translations;
